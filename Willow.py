f178cc8fa843fb5bdbeb563daf207a6c808b3e00
Thomas <ezra2391@gmail.com>
2025-11-26 12:02:20 +0100

    Co-authored-by: Mifteack <Mifteack@users.noreply.github.com>

--- Willow.py
+++ Willow.py
@@ -1,44 +1,112 @@
-Player = "Salle0"
+import random
+L_Salles = [0,1,2,3,4,5,6,7,8,9,10,11]
 
+def init_game():
+    
+    # Positions initiales
+    Player = random.choice(L_Salles)
+    Willow =  random.choice(L_Salles)
+    while Willow == Player:
+        Willow =  random.choice(L_Salles)
+    while C1 == Player or C1 == Willow: 
+        C1 = random.choice(L_Salles)
+    while C2 == Player or C2 == Willow or C2 == C1:
+        C2 =  random.choice(L_Salles)
+    return Willow, C1, C2
+
+# Salle de 0 à 11, commençant au W (sur l'image dans les consignes thomas)
 def Joueur(Player):
-    if Player == "Salle0":
-        print ("Vous pouvez vous déplacer dans les salles 6, 9, et 12")
+    if Player == 0 :
+        print ("Vous pouvez vous déplacer dans les salles 1, 5, et 6")
         Move = int(input("Quelle salle choisissez vous ?"))
-        if Move == 1 or Move == 9 or Move == 12:
-            Player = str("Salle"+Move)
-            print("Vous êtes maintenant dans la",Player)
+        if Move == 1 or Move == 5 or Move == 6:
+            Player = Move
+            print("Vous êtes maintenant dans la Salle",Move)
             return(Player)
         else :
             print ("Erreur veuillez saisir à nouveau")
             return Joueur(Player)
-    if Player == "Salle1":
+    if Player == 1 :
         return
-    if Player == "Salle2":
+    elif Player == 2 :
         return
-    if Player == "Salle3":
+    elif Player == 3 :
         return
-    if Player == "Salle4":
+    elif Player == 4 :
         return
-    if Player == "Salle5":
+    elif Player == 5 :
         return
-    if Player == "Salle6":
+    elif Player == 6 :
         return
-    if Player == "Salle7":
+    elif Player == 7 :
         return
-    if Player == "Salle8":
+    elif Player == 8 :
         return
-    if Player == "Salle9":
+    elif Player == 9 :
         return
-    if Player == "Salle10":
+    elif Player == 10 :
         return
-    if Player == "Salle11":
+    elif Player == 11 :
         return
-    if Player == "Salle12":
+
+def check_positions():
+    if Player == Willow
+        print("Vous avez  !")
         return
-    
-Joueur("Salle0")
+    if Player == C1 or Player == C2:
+        TP = random.choice(L_Salles) 
+        Player = TP
+        if Player == Willow
+            print("Vous avez été téléporté sur le Willow, il dévore tout vos gateaux!")
+            return
+
 
 
 # check si Player est sur méchant ou si méchant et proche etc ...
 
 # def méchant
+
+# def Menu1():
+#     print("1. Vous deplacer ?")
+#     print("2. Poser le spike ?")
+#     print("3. Afficher les réactions d'une enzyme")
+#     print("4. Afficher les réactions d'un métabolite")
+#     print("5. Quitter")
+
+# while True:
+#     Menu1()
+#     rep= int(input("que voulez vous faire ?"))
+#     if rep==1:
+#         creerReaction()
+#     elif rep==2:
+        
+#     elif rep==3:
+#         query=input("Quelle enzyme recherchez-vous ?")
+#         afficherReaction(query)
+#     elif rep==4:
+#         querymeta=input("Quel métabolite recherchez-vous ?")
+#         affichemetabolite(querymeta)
+#     elif rep==5:
+#         break
+
+# def Menu2():
+#     print("1. Vous deplacer ?")
+#     print("2. Afficher les réactions d'une enzyme")
+#     print("3. Afficher les réactions d'un métabolite")
+#     print("4. Quitter")
+
+# while True:
+#     Menu2()
+#     rep= int(input("que voulez vous faire ?"))
+#     if rep==1:
+#         creerReaction()
+#     elif rep==2:
+        
+#     elif rep==3:
+#         query=input("Quelle enzyme recherchez-vous ?")
+#         afficherReaction(query)
+#     elif rep==4:
+#         querymeta=input("Quel métabolite recherchez-vous ?")
+#         affichemetabolite(querymeta)
+#     elif rep==5:
+#         break
